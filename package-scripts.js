701c8d6d5eeac8434fb7b04ed59c6db71e6ca36b
Walaff Fernandes <walaffonofre@icloud.com>
2022-06-04 13:38:12 -0300

    docs: :zap: configurando os scripts com o nps

--- package-scripts.js
+++ package-scripts.js
@@ -1,57 +1,74 @@
 const path = require("path");
 
-const apiPath = path.resolve(__dirname, "apps/api");
+const purchasesPath = path.resolve(__dirname, "apps/purchases");
+const classroomPath = path.resolve(__dirname, "apps/classroom");
 const webPath = path.resolve(__dirname, "apps/web");
 
-const ciApiPath = path.resolve(__dirname, "out/apps/api");
+const ciPurchasesPath = path.resolve(__dirname, "out/apps/purchases");
+const ciClassroomPath = path.resolve(__dirname, "out/apps/classroom");
 const ciWebPath = path.resolve(__dirname, "out/apps/web");
 
 module.exports = {
   scripts: {
     prepare: {
-      default: `nps prepare.web prepare.api`,
+      default: `nps prepare.web prepare.services`,
       web: `yarn`,
-      api: `nps prepare.docker prisma.migrate.dev`,
+      services: `nps prepare.docker prepare.prisma`,
       docker: "docker-compose up -d",
       ci: {
         web: `npx turbo prune --scope=web && cd out && yarn install --frozen-lockfile`,
-        api: `npx turbo prune --scope=api && cd out && yarn install --frozen-lockfile && nps prisma.generate`,
+        purchases: `npx turbo prune --scope=purchases && cd out && yarn install --frozen-lockfile && nps prisma.generate`,
       },
+      prisma: `nps prisma.purchases.migrate.dev prisma.classroom.migrate.dev`,
     },
     test: {
-      default: `nps test.web test.api`,
+      default: `nps test.web test.purchases`,
       web: `cd ${webPath} && yarn test:watch`,
-      api: `cd ${apiPath} && yarn test:watch`,
+      purchases: `cd ${purchasesPath} && yarn test:watch`,
+      classroom: `cd ${classroomPath} && yarn test:watch`,
       ci: {
-        default: `nps test.ci.web test.ci.api`,
+        default: `nps test.ci.web test.ci.purchases`,
         web: `cd ${ciWebPath} && yarn test:ci`,
-        api: `cd ${ciApiPath} && yarn test:ci`,
+        purchases: `cd ${ciPurchasesPath} && yarn test:ci`,
+        classroom: `cd ${ciClassroomPath} && yarn test:ci`,
       },
       watch: {
-        default: `nps test.watch.web test.watch.api`,
+        default: `nps test.watch.web test.watch.purchases test.watch.classroom`,
         web: `cd ${webPath} && yarn test:ci`,
-        api: `cd ${apiPath} && yarn test:watch`,
+        purchases: `cd ${purchasesPath} && yarn test:watch`,
+        classroom: `cd ${classroomPath} && yarn test:watch`,
       },
     },
     prisma: {
-      generate: `cd ${apiPath} && npx prisma generate`,
-      studio: `cd ${apiPath} && npx prisma studio`,
-      migrate: {
-        dev: `cd ${apiPath} && npx prisma migrate dev`,
+      purchases: {
+        generate: `cd ${purchasesPath} && yarn prisma generate`,
+        studio: `cd ${purchasesPath} && yarn prisma studio`,
+        migrate: {
+          dev: `cd ${purchasesPath} && yarn prisma migrate dev`,
+        },
       },
+      classroom: {
+        generate: `cd ${classroomPath} && yarn prisma generate`,
+        studio: `cd ${classroomPath} && yarn prisma studio`,
+        migrate: {
+          dev: `cd ${classroomPath} && yarn prisma migrate dev`,
+        },
+      }
     },
     build: {
       default: "npx turbo run build",
       ci: {
         web: "cd out && npm run build",
-        api: "cd out && npm run build",
+        purchases: "cd out && npm run build",
+        classroom: "cd out && npm run build",
       },
     },
     docker: {
       build: {
-        default: "nps docker.build.web docker.build.api",
+        default: "nps docker.build.web docker.build.purchases docker.build.classroom",
         web: `docker build -t web . -f ${webPath}/Dockerfile`,
-        api: `docker build -t api . -f ${apiPath}/Dockerfile`,
+        purchases: `docker build -t purchases . -f ${purchasesPath}/Dockerfile`,
+        classroom: `docker build -t classroom . -f ${classroomPath}/Dockerfile`,
       },
     },
     dev: "npx turbo run dev",
