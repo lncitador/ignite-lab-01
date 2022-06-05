fa43a376143baab202225957a636eec0b402e841
Walaff Fernandes <walaffonofre@icloud.com>
2022-06-05 02:17:56 -0300

    feat(purchases): :zap: inversão de dependencia
    foi criado o provider do repositorio do prisma para que haja uma inversão de dependecia

---
+++ apps/purchases/src/modules/purchases/domain/constants/provider.ts
@@ -0,0 +1,3 @@
+export const PROVIDER = {
+  PURCHASES_REPOSITORY: 'PurchasesRepository',
+};
--- apps/purchases/src/modules/purchases/purchases.module.ts
+++ apps/purchases/src/modules/purchases/purchases.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
+import { PROVIDER } from './domain/constants/provider';
+import { PurchasesPrismaRepository } from './infrastructure/repository/prisma/purchases.repository';
+import { PurchasesResolver } from './infrastructure/resolver/purchases.resolver';
+import { CreatePurchasesUseCase } from './usecases/create-purchases.usecase';
 
-@Module({})
+const PROVIDERS: Provider[] = [
+  {
+    provide: PROVIDER.PURCHASES_REPOSITORY,
+    useClass: PurchasesPrismaRepository,
+  },
+];
+
+@Module({
+  providers: [...PROVIDERS, PurchasesResolver, CreatePurchasesUseCase],
+  exports: [...PROVIDERS],
+})
 export class PurchasesModule {}
